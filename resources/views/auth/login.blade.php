b562b8d4fc1c6d3d054568c5d578d2f72f20c6db
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2021-02-24 17:57:56 +0200

    edit in login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     
-    <link rel="stylesheet" href="{{ URL::asset('resources/css/style.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('resources/assets/css/style.css') }}">
 </head>
 <body>
 
