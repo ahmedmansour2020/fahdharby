6b68ab0132f0e5d3352c6fd6e92dadcbf616b3c6
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2021-04-02 16:05:04 +0200

    create home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,128 @@
-{{--
-@extends('layout.layout')
-@section('title','Home')
---}}
-<?php
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
-$lang=LaravelLocalization::setLocale();
-?>
+@extends('layouts.layout')
+
+@section('title', isset($title) ? $title : '')
 @section('content')
-<div class="container">
-Hello
-</div>
-{{--
+@section('home')
+    <?php $home = true; ?>
 @endsection
-@section('page_js')
+
+<div class="container-fluid px-5">
+        <div class="siteWidthContainer">
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Group 266.png') }}" class="img-fluid" alt="">
+                        جميع التصنيفات
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        موبايلات وتابلت
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        تيليفزيونات
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        إلكترونيات
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        المنزل و المطبخ
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        أجهزة منزلية
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        الصحة والجمال
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        الألعاب
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        الأمهات والأطفال
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        الرياضة
+                    </a>
+                </div>
+            </div>
+            <div class="bannerContainer">
+                <div>
+                    <a href="#">
+                        <img src="{{ URL::asset('resources/assets/images/Mask Group 20.png') }}" class="img-fluid"
+                            alt="">
+                        الغذاء والمقبلات
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="fotorama text-center slide-header d-flex justify-content-center"  data-autoplay="true">
+            <div  data-img="{{ URL::asset('resources/assets/images/Group 269.png') }}"><span></span></div>
+            <div  data-img="{{ URL::asset('resources/assets/images/Group 269.png') }}"><span></span></div>
+            <div  data-img="{{ URL::asset('resources/assets/images/Group 269.png') }}"><span></span></div>
+
+            {{-- <img src="{{ URL::asset('resources/assets/images/Group 269.png') }}" class="img-fluid d-block w-100"
+                alt="">
+            <img src="{{ URL::asset('resources/assets/images/Group 269.png') }}" class="img-fluid d-block w-100"
+                alt=""> --}}
+
+        </div>
+    </div>
+</div>
+
+
 
 @endsection
---}}
