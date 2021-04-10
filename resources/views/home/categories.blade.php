cca76256d5b7f1f4b3dc09aba1fb0384c989db08
ahmedmansour2020 <al3etreby_92@yahoo.com>
2021-04-10 21:16:38 +0200

    fix sliders,manage categories and products

---
+++ resources/views/home/categories.blade.php
@@ -0,0 +1,39 @@
+<?php
+use App\Http\Controllers\UserController;
+?>
+@extends('layouts.layout')
+@section('title',isset($title)?$title:'')
+@section('content') 
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 px-0">
+                <div class="header-sub-category mt-5" @if(isset($main_category)) style="background-image: url('{{$main_category->image}}')" @endif>
+                    <h2>{{$title}}</h2>
+                </div>
+            </div>
+            @if(isset($main_category))
+            @foreach($sub_categories as $category)
+            <div class="col-sm-6 col-md-4">
+                <a href="{{route('products',$category->id)}}" class="box-home-category" >
+                    <img src="{{$category->image}}" style="max-height:300px" class="img-fluid" alt="">
+                    <h3>{{$category->name}}</h3>
+                </a>
+            </div>
+            @endforeach
+            @else
+            @foreach($categories as $category)
+            <div class="col-sm-6 col-md-4">
+                <a href="{{route('categories',$category->id)}}" class="box-home-category" >
+                    <img src="{{$category->image}}" style="max-height:300px" class="img-fluid" alt="">
+                    <h3>{{$category->name}}</h3>
+                </a>
+            </div>
+            @endforeach
+            @endif
+        </div>
+    </div>
+
+
+    
+@endsection
