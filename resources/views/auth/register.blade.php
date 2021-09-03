347301b12832addb7fd1d72b90ebf7caef72891d
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2021-09-03 13:40:50 +0200

    edit responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +8,20 @@
     <title>register</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
     <link rel="stylesheet" href="{{ URL::asset('resources/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('resources/assets/css/media.css') }}">
 </head>
+
 <body>
 
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <form action="{{route('register')}}" class="form-login" method="POST">
-                @csrf
+                <form action="{{ route('register') }}" class="form-login" method="POST">
+                    @csrf
                     <div class="card">
                         <div class="card-header">
                             <h2>تسجيل الدخول كمستخدم جديد</h2>
@@ -30,31 +34,37 @@
                                 <input type="email" class="w-100" name="email" placeholder="البريد الألكتروني">
                             </div>
                             <div class="form-group">
-                                <input type="password" class="w-100" name="password" placeholder=" كلمة المرور">
+                                <input type="password" class="w-100" name="password"
+                                    placeholder=" كلمة المرور">
                             </div>
                             <div class="form-group">
-                                <input type="password" class="w-100" name="password_confirmation" placeholder="تأكيد كلمة المرور">
+                                <input type="password" class="w-100" name="password_confirmation"
+                                    placeholder="تأكيد كلمة المرور">
                             </div>
                             <div class="row">
-                            @foreach(App\Http\Controllers\DashboardController::genders() as $gender)
-                                <div class="col-6">
-                                    <div class="group-radio">
-                                        <input type="radio" name="gender" value="{{$gender->id}}">
-                                        <label for="gender">{{$gender->name}}</label>
+                                @foreach (App\Http\Controllers\DashboardController::genders() as $gender)
+                                    <div class="col-6">
+                                        <div class="group-radio">
+                                            <input type="radio" name="gender" value="{{ $gender->id }}">
+                                            <label for="gender">{{ $gender->name }}</label>
+                                        </div>
                                     </div>
-                                </div>
-                            @endforeach
+                                @endforeach
                             </div>
                             <button type="submit" class="btn btn-primary">أنشئ حسابك</button>
                             <p class="or">او</p>
                             <div class="social-form-register text-center">
-                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 105.png') }}" class="img-fluid pl-2" alt=""></a>
-                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 107.png') }}" class="img-fluid pl-2" alt=""></a>
-                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 106.png') }}" class="img-fluid pl-2" alt=""></a>
-                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 116.png') }}" class="img-fluid" alt=""></a>
+                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 105.png') }}"
+                                        class="img-fluid pl-2" alt=""></a>
+                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 107.png') }}"
+                                        class="img-fluid pl-2" alt=""></a>
+                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 106.png') }}"
+                                        class="img-fluid pl-2" alt=""></a>
+                                <a href="#"><img src="{{ URL::asset('resources/assets/images/Group 116.png') }}"
+                                        class="img-fluid" alt=""></a>
                             </div>
                             <div class="register-login text-center">
-                                <p> هل لديك حساب بالفعل ! <a href="{{route('login')}}">سجل الان</a> </p>
+                                <p> هل لديك حساب بالفعل ! <a href="{{ route('login') }}">سجل الان</a> </p>
                             </div>
 
                         </div>
@@ -63,10 +73,17 @@
             </div>
         </div>
     </div>
-    
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
+        integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
+    </script>
     <script></script>
 </body>
+
 </html>
--- resources/views/layouts/navbar-home.blade.php
+++ resources/views/layouts/navbar-home.blade.php
@@ -82,58 +82,66 @@
 
 </nav> --}}
 
-<nav class="navbar navbar-expand-lg navbar-bg-white">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <div class="row w-100">
-            <div class="col-12">
-                <a class="navbar-brand" href="{{ route('home') }}">
-                    <img src="{{URL::asset('resources/assets/images/logo.png')}}" class="img-fluid" alt="Logo">
-                </a>
-                <button class="navbar-toggler bg-dark text-left float-left" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon bg-light"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img src="{{ URL::asset('resources/assets/images/logo.png') }}" class="img-fluid" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                    <a href="{{route('home')}}" class="link-home">الرئيسية</a>
-                    <form action="{{route('search')}}" method="GET" class="search ">
-                        <input type="text" name="search" placeholder="ابحث  عن المنتج الذي تريده">
-                        <div class="search-icon  h-100 position-absolute d-flex justify-content-center align-items-center" style="top:0;left:0">
-                            <!-- <img src="{{URL::asset('resources/assets/images/search.png')}}" class="img-fluid" alt=""> -->
-                            <button type="submit" class="btn btn-transparent"><i class="fa fa-search text-primary"></i></button>
-                        </div>
-                    </form>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <ul class="navbar-nav mr-auto">
-                        @guest
-                        <a href="#" class="btn btn-primary login btn-chart ml-3">تسجيل دخول</a>
-                        <a href="#" class="btn btn-chart bg-white border-primary ml-3 register-modal" style="color: #306EFF">حساب جديد</a>
-                        @endguest
-                        @auth
-                        <?php
-                        $user = auth()->user();
-                        ?>
-                        <li class="nav-item   dropdown">
-                            <a class="nav-link nav-name-users  dropdown-toggle d-inline-block " style="font-size: 19px;font-weight: 700;" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span id="ahmed">
-                                    {{$user->name}}
-                                </span>
-                            </a>
-                            <img src="{{URL::asset('resources/assets/images/Group 1225.png')}}" class="img-fluid px-1 d-inline-block" style="width: 25%" alt="">
-
-                            <div class="dropdown-menu text-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{route($user->role_id==1?'admin':($user->role_id==2?'vendor':'my-account'))}}"><i class="fas fa-user-alt pl-2 "></i>حسابي </a>
-                                <a class="dropdown-item logout" href=""><i class="fa fa-sign-out-alt pl-2 logout "></i>تسجيل الخروج </a>
-                            </div>
-                        </li>
-                        @endauth
-                    </ul>
-
-                    @auth
-                    <a href="{{route('cart')}}" class="btn btn-primary btn-chart">سلة الشراء <img src="{{URL::asset('resources/assets/images/shopping-chart.png')}}" class="img-fluid" alt=""></a>
-                    @endauth
+            <a href="{{ route('home') }}" class="link-home">الرئيسية</a>
+            <form action="{{ route('search') }}" method="GET" class="search ">
+                <input type="text" name="search" placeholder="ابحث  عن المنتج الذي تريده">
+                <div class="search-icon  h-100 position-absolute d-flex justify-content-center align-items-center"
+                    style="top:0;left:0">
+                    <!-- <img src="{{ URL::asset('resources/assets/images/search.png') }}" class="img-fluid" alt=""> -->
+                    <button type="submit" class="btn btn-transparent"><i class="fa fa-search text-primary"></i></button>
                 </div>
-            </div>
+            </form>
+
+            <ul class="navbar-nav">
+                @guest
+                    <a href="#" class="btn btn-primary login btn-chart ml-3">تسجيل دخول</a>
+                    <a href="#" class="btn btn-chart bg-white border-primary ml-3 register-modal"
+                        style="color: #306EFF">حساب جديد</a>
+                @endguest
+                @auth
+                    <?php
+                    $user = auth()->user();
+                    ?>
+                    <li class="nav-item   dropdown">
+                        <a class="nav-link nav-name-users  dropdown-toggle d-inline-block "
+                            style="font-size: 19px;font-weight: 700;" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span id="ahmed">
+                                {{ $user->name }}
+                            </span>
+                        </a>
+                        <img src="{{ URL::asset('resources/assets/images/Group 1225.png') }}"
+                            class="img-fluid px-1 d-inline-block" style="width: 25%" alt="">
+
+                        <div class="dropdown-menu text-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item"
+                                href="{{ route($user->role_id == 1 ? 'admin' : ($user->role_id == 2 ? 'vendor' : 'my-account')) }}"><i
+                                    class="fas fa-user-alt pl-2 "></i>حسابي </a>
+                            <a class="dropdown-item logout" href=""><i class="fa fa-sign-out-alt pl-2 logout "></i>تسجيل
+                                الخروج </a>
+                        </div>
+                    </li>
+                @endauth
+            </ul>
+
+            @auth
+                <a href="{{ route('cart') }}" class="btn btn-primary btn-chart">سلة الشراء <img
+                        src="{{ URL::asset('resources/assets/images/shopping-chart.png') }}" class="img-fluid"
+                        alt=""></a>
+            @endauth
         </div>
     </div>
 
